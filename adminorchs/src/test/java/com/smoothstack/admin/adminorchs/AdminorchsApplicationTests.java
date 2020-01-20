7cda7eff0e1820dee0a1c083056a5702ec4a9c6a
Lee Raulin <leeraulin@gmail.com>
2020-01-20 16:20:02 -0500

    first commit

---
+++ adminorchs/src/test/java/com/smoothstack/admin/adminorchs/AdminorchsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smoothstack.admin.adminorchs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminorchsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
