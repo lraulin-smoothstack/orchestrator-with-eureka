d2e55fc7fa9b440c5c34d848631206b68049291b
timothy-breitenfeldt <timothy.breitenfeldt@smoothstack.com>
2020-01-21 15:18:41 -0500

    fixed bug where missing @Service anotation on services.

--- orchestrator/src/main/java/com/smoothstack/december/orchestrationservice/service/RoleService.java
+++ orchestrator/src/main/java/com/smoothstack/december/orchestrationservice/service/RoleService.java
@@ -3,10 +3,12 @@ package com.smoothstack.december.orchestrationservice.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.smoothstack.december.orchestrationservice.dao.RoleDAO;
 import com.smoothstack.december.orchestrationservice.entity.Role;
 
+@Service
 public class RoleService {
 
     @Autowired
--- orchestrator/src/main/java/com/smoothstack/december/orchestrationservice/service/UserService.java
+++ orchestrator/src/main/java/com/smoothstack/december/orchestrationservice/service/UserService.java
@@ -3,10 +3,12 @@ package com.smoothstack.december.orchestrationservice.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.smoothstack.december.orchestrationservice.dao.UserDAO;
 import com.smoothstack.december.orchestrationservice.entity.User;
 
+@Service
 public class UserService {
 
     @Autowired
